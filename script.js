7d8fdf2be9e36ae850cde6883f7cdf3756c9fbd3
Ignacio <ignacio.habiague1@gmail.com>
2025-01-09 14:58:19 -0300

    operate function created

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function add(a, b) { return a + b; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
+
+function operate(operator, a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch(operator) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+    }
+}
